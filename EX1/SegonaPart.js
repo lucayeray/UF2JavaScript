0abe37130f1f8ad0df7a03e86c9542f53c39df1e
Luca Yeray <lucayeray.andrade@institutvidreres.cat>
2023-10-30 22:14:30 +0100

    Ex1

---
+++ EX1/SegonaPart.js
@@ -0,0 +1,14 @@
+
+//EX6
+var number = 1.234;
+var BigInt = 5.678;
+
+var suma = number + BigInt;
+var resta = number - BigInt;
+var divisio = number / BigInt;
+var mult = number * BigInt;
+
+console.log("La suma és: " + suma.toFixed(1));
+console.log("La resta és: " + resta.toFixed(1));
+console.log("La divisió és: " + divisio.toFixed(1));
+console.log("La multiplicació és: " + mult.toFixed(1));
