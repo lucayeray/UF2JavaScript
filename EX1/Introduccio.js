81e698c3126476f1d40e87497661070d4dee87a2
Luca Yeray <lucayeray.andrade@institutvidreres.cat>
2023-10-30 22:00:00 +0100

    Ex1

--- EX1/Introduccio.js
+++ EX1/Introduccio.js
@@ -1,11 +1,32 @@
 
+/*
+
 //Ex1
 console.log("Nom: Luca Yeray")
 console.log("Cognom: Andrade Sosa")
 console.log("Data Naixement: 18/09/2005")
+*/
 
 //Ex2
-
+/*
 var nom = "Luca";
 var edat = 18;
 var estudia = true;
+*/
+
+//Ex3
+/*
+console.log(hola)
+let hola = "Holaa";
+ */
+
+//Ex4
+/*
+var x = 20;
+x = "Hola";
+console.log(x);
+ */
+
+//Ex5
+
+
